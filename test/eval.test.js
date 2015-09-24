7d45cc2b84610aab8215cb8b6206e7f6e5609186
Henri Bergius <henri.bergius@iki.fi>
2015-09-24 12:57:41 -0400

    Make linter happy

--- test/eval.test.js
+++ test/eval.test.js
@@ -1,18 +1,18 @@
 
 var assert = require('better-assert')
-var run = require('..')
+var run = require('..');
 
 it('should have a useful `require`', function(){
-  var m = run('module.exports = require("..")', __filename)
-  assert(m.exports == run)
-})
+  var m = run('module.exports = require("..")', __filename);
+  assert(m.exports == run);
+});
 
 it('should run in this context', function(){
-  var m = run('module.exports = function(){}', __filename)
-  assert(m.exports instanceof Function)
-})
+  var m = run('module.exports = function(){}', __filename);
+  assert(m.exports instanceof Function);
+});
 
 it('should provide access to the return value', function(){
-  var m = run('1;2;3')
-  assert(m.return == 3)
-})
+  var m = run('1;2;3');
+  assert(m.return == 3);
+});
