fb66ad05796d96b39742fcf38b090efed16f4f63
Henri Bergius <henri.bergius@iki.fi>
2015-09-24 13:02:24 -0400

    Add a test with a fake path

--- test/eval.test.js
+++ test/eval.test.js
@@ -1,6 +1,6 @@
-
-var assert = require('better-assert')
+var assert = require('better-assert');
 var run = require('..');
+var path = require('path');
 
 it('should have a useful `require`', function(){
   var m = run('module.exports = require("..")', __filename);
@@ -10,6 +10,13 @@ it('should have a useful `require`', function(){
 it('should run in this context', function(){
   var m = run('module.exports = function(){}', __filename);
   assert(m.exports instanceof Function);
+});
+
+it('should be able to run external requires', function (){
+  var context = path.resolve(__dirname, '../foo.js');
+  var m = run('var an = require(\'./test/fixtures/another\');module.exports = function (a) { return an(a + 1) };', context);
+  var val = m.exports(2);
+  assert(val == 6);
 });
 
 it('should provide access to the return value', function(){
